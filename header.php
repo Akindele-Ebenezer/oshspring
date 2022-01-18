875aaf73a0720fd93000082b195f15ca8af17ce4
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-01-18 13:19:08 +0100

    Create header.php

---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet"> 
+    <title>OSHSPRING MULTIREQUEST ENTERPRISES</title>
+</head>
+<body>
+    
+<div class="load">
+        <div>
+            <p><i><b>Oshspring</b></i></p>
+            <div>
+                <div class="circles"></div>
+                <div class="circles"></div>
+                <div class="circles"></div>
+            </div>
+        </div>
+    </div>  
+    
+<section class="main">
+    
+<nav>
+    <ul>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#details">ABOUT</a></li>
+        <li><a href="#services">SERVICES</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+    </ul>    
+</nav>
+
+    <div class="nav">
+        <div>
+            <div>
+                <input type="search" placeholder="Search">
+            </div>
+            <div>
+                <img src="images/nav-icon.PNG">
+            </div>
+        </div>
+    </div>
+
+<main>
+   
