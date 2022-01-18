75f09874e272da25549dee4f86508af56a31241c
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-01-18 07:56:39 +0100

    Update message-success.php

--- message-success.php
+++ message-success.php
@@ -90,11 +90,11 @@ if(isset($_POST["submit"])) {
         <div>
             <h1>Useful Links</h1>
             <ul>
-                <li>HOME</li>
-                <li>ABOUT</li>
-                <li>SERVICES</li>
-                <li>CONTACT</li>
-                <li><a href="https://api.whatsapp.com/send?phone=2348023704304">WHATSAPP</a></li>
+                <li class="footer-links">HOME</li>
+                <li class="footer-links">ABOUT</li>
+                <li class="footer-links">SERVICES</li>
+                <li class="footer-links">CONTACT</li>
+                <li class="footer-links"><a href="https://api.whatsapp.com/send?phone=2348023704304">WHATSAPP</a></li>
             </ul>           
         </div>
 
