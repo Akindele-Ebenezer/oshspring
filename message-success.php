832783dfa6e5426814d513dedcf0fef9db88bfc6
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-01-18 13:22:15 +0100

    Update message-success.php

--- message-success.php
+++ message-success.php
@@ -1,5 +1,6 @@
 <?php
 include "config.php";
+include "header.php";
 if(isset($_POST["submit"])) {
     $first_name = $_POST["first_name"];
     $last_name = $_POST["last_name"];
@@ -12,43 +13,6 @@ if(isset($_POST["submit"])) {
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles/styles.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet"> 
-    <title>Message Sent</title>
-</head>
-<body>
-    
-<section class="main">
-        
-<nav>
-    <ul>
-        <li><a href="#">HOME</a></li>
-        <li><a href="#">ABOUT</a></li>
-        <li><a href="#">SERVICES</a></li>
-        <li><a href="#contact">CONTACT</a></li>
-    </ul>    
-</nav>
-
-    <div class="nav">
-        <div>
-            <div>
-                <input type="search" placeholder="Search">
-            </div>
-            <div>
-                <img src="images/nav-icon.PNG">
-            </div>
-        </div>
-    </div>
-
-<main>
 <div class="message-success">
     <div>
         <h1>Message Sent</h1>
@@ -56,59 +20,6 @@ if(isset($_POST["submit"])) {
     </div>
 </div>
 
-
-<footer>
-    <div>
-        <div>
-            <h1>OSHSPRING MultiRequest Enterprise</h1>
-            <p>We plan, implement, and control the movement and storage of goods, services, or information within Nigeria.
-            <br><br>
-            Moving equipments/materials for your new House, Church, School or any other Organisations from one Location to other Locations safely is our Job.
-            </p>
-            <br>
-            <div>
-                <img src="#" alt="Whatsapp">
-                <img src="#" alt="Facebook">
-                <img src="#" alt="IG">
-                <img src="#" alt="Gmail">
-            </div>
-        </div>
-
-        
-        <div>
-            <h1>SERVICES</h1>
-            <ul>
-                <li>Transportation</li>
-                <li>Warehousing</li>
-                <li>Information</li>
-                <li>Material Handling</li>
-                <li>Inventory</li>
-                <li>Warehousing</li>
-            </ul>           
-        </div>
-        
-        <div>
-            <h1>Useful Links</h1>
-            <ul>
-                <li class="footer-links">HOME</li>
-                <li class="footer-links">ABOUT</li>
-                <li class="footer-links">SERVICES</li>
-                <li class="footer-links">CONTACT</li>
-                <li class="footer-links"><a href="https://api.whatsapp.com/send?phone=2348023704304">WHATSAPP</a></li>
-            </ul>           
-        </div>
-
-        
-        <div>
-            <h1>ADDRESS</h1>
-            <p>Adress/Location</p>
-            <p>Lagos, Nigeria</p>           
-        </div>
-    </div>
-</footer>
-<p>All Rights reserved by OSHSPRING MultiRequest Enterprise &copy;2021</p>
-<script src="scripts/scripts.js"></script> 
-</main>
-</section>
-</body>
-</html>
+<?php
+    include "footer.php";
+?>
