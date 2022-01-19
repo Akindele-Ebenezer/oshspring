8ab0bb24a554490edaba3da7f4c38f8c20c74e1d
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-01-19 06:14:15 +0100

    Add files via upload

--- admin-login.php
+++ admin-login.php
@@ -1,12 +1,41 @@
+
 <?php
   $title = "Admin";
   include "header.php";
-?>
+  include "config.php";
 
-<form action="<?= $_SERVER['PHP _SELF'] ?>" method="get" class="admin-login">
+  if(isset($_POST["submit"])) {
+
+    $username = $_POST["username"]; 
+    $password = $_POST["password"]; 
+
+    $sql = "SELECT * FROM admin WHERE username = 'admin' AND password = 'adminkemi123';";
+    $query = mysqli_query($conn, $sql);
+
+    if(mysqli_num_rows($query) == 1) {
+      session_start();
+      $_SESSION["username"] = true;
+      header('Location: admin-inbox.php');
+    } 
+
+  }
+
+?>
+<style>
+  .admin-login {
+    display: flex;
+    justify-content: center;
+    padding: 5rem;
+  }
+
+  .admin-login h1 {
+    color: var(--yellow);
+  }
+</style>
+<form action="<?= $_SERVER['PHP_SELF'] ?>" method="post" class="admin-login">
     <div>
         <h1>Log In</h1>
-        <input type="text" name="email" placeholder="Email">
+        <input type="text" name="username" placeholder="Enter Username">
         <span></span>
         <input type="password" name="password" placeholder="Password">
         <span></span>
