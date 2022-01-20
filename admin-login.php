ee4cdb26b59e9b312b2a494882c3826ee14b0f67
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-01-20 21:23:52 +0100

    Add files via upload

--- admin-login.php
+++ admin-login.php
@@ -1,18 +1,27 @@
 
 <?php
+  error_reporting(0);
   include "config.php";
   $title = "Admin";
   include "header.php";
 
   if(isset($_POST["submit"])) {
 
-    $username = $_POST["username"]; 
-    $password = $_POST["password"]; 
+    $username = mysqli_real_escape_string($conn, $_POST["username"]); 
+    $password = mysqli_real_escape_string($conn, $_POST["password"]); 
 
     $sql = "SELECT * FROM admin WHERE username = '$username' AND password = '$password';";
     $query = mysqli_query($conn, $sql);
     $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
     print_r($result);
+
+    if(empty($username)) {
+      $username_error = "Enter Username";
+    } elseif(empty($password)) {
+      $password_error = "Enter Password";
+    } else {
+      $wrong_credentials = "Wrong Username/Password";
+    }
 
     if(mysqli_num_rows($query) == 1) {
       session_start();
@@ -22,15 +31,20 @@
 
   }
 
-?> 
-
+?>
+<style> 
+  .admin-login span {
+    color: red;
+  }
+</style>
 <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post" class="admin-login">
-    <div>
+    <div> 
+        <span><?= $wrong_credentials; ?></span>
         <h1>Log In</h1>
-        <input type="text" name="username" placeholder="Enter Username" class="admin-login-input">
-        <span></span>
-        <input type="password" name="password" placeholder="Password" class="admin-login-input">
-        <span></span>
+        <input type="text" name="username" placeholder="Enter Username"><br>
+        <span><?= $username_error; ?></span>
+        <input type="password" name="password" placeholder="Password"><br>
+        <span><?= $password_error; ?></span>
         <br><br>
         <button type="submit" name="submit">Submit</button>
     </div>
