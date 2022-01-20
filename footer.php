96d1e57e56dddc1697c8d08f9a66b3b7596256fd
Akindele-Ebenezer <83076721+Akindele-Ebenezer@users.noreply.github.com>
2022-01-20 13:19:20 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -37,7 +37,7 @@
                             <li class="footer-links">SERVICES</li>
                             <li class="footer-links">CONTACT</li>
                                 <li class="footer-links"><a href="admin-login.php">ADMIN</a></li>
-                            <li><a href="https://api.whatsapp.com/send?phone=2348023704304">WHATSAPP</a></li>
+                            <li class="footer-links"><a href="https://api.whatsapp.com/send?phone=2348023704304">WHATSAPP</a></li>
                         </ul>           
                     </div>
 
